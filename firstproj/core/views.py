f7e2e585e5a14ae5483a6eae8ee3f39dee1dc76e
MDyeimison <marcosdyeimison@gmail.com>
2022-11-04 13:02:17 -0300

    update

--- firstproj/core/views.py
+++ firstproj/core/views.py
@@ -1,8 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
+
+from .models import Choice, Question
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello human!!!")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return render(request, 'core/index.html', context)
+
+""" def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'core/details.html', {'question': question}) """
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'core/detail.html', {'question': question})
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'core/results.html', {'question': question})
+
+## _set = consultar objetos nao ligados diretamente (reverse querry)
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, 'core/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+    return HttpResponseRedirect(reverse('core:results', args=(question.id,)))
